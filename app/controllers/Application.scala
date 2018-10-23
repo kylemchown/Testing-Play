4e8983cee7ecbceda87470c30d02e90042c0d013
kylemchown <kyle.m.chown@gmail.com>
2018-10-23 09:03:06 +0100

    Added redirect and urls with variables

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,20 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Your new srdtgh is ready."))
+  }
+
+  def copyindex = Action {
+    Ok(views.html.copyindex("srjjiehjeksjrhb"))
+  }
+
+  def copyind(id: Long = 12345) = Action {
+    Ok(views.html.copyindex(id.toString))
+  }
+
+  def toHome= Action { implicit request:
+  Request[AnyContent] =>
+    Redirect("/")
   }
 
 }
